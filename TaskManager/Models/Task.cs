79122c201f343e360393c60aee6334b106591c88
Carlos Betancourt Villada <cbetancourt06@hotmail.com>
2023-07-03 23:00:27 -0600

    First commit
    First commit

---
+++ TaskManager/Models/Task.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace TaskManager.Models
+{
+    public enum Status
+    {
+        New = 1,
+        [Description("In Progress")]
+        InProgress = 2,
+        Completed = 3,
+
+    }
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public Status Status { get; set; }
+
+    }
+}
---
+++ TaskManager/Models/TasksContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TaskManager.Models
+{
+    public class TasksContext : DbContext
+    {
+        public TasksContext(DbContextOptions<TasksContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Task> TodoItems { get; set; } = null!;
+    }
+}
